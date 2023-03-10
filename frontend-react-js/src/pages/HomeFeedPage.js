fd75a827d405c2e57484a39dcbf3525e6a51b704
babs34 <oluwoleadubiobi@gmail.com>
2023-03-10 15:21:55 +0000

    aythenticat

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -10,7 +10,7 @@ import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
 
 // [TODO] Authenication
-
+import Cookies from 'js-cookie'
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -19,12 +19,14 @@ export default function HomeFeedPage() {
   const [replyActivity, setReplyActivity] = React.useState({});
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
-  
 
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
-      const res = await fetch(backend_url,{
+      const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
         method: "GET"
       });
       let resJson = await res.json();
@@ -38,62 +40,58 @@ export default function HomeFeedPage() {
     }
   };
 
- 
-// check if we are authenicated
-const checkAuth = async () => {
-  Auth.currentAuthenticatedUser({
-    // Optional, By default is false. 
-    // If set to true, this call will send a 
-    // request to Cognito to get the latest user data
-    bypassCache: false 
-  })
-  .then((user) => {
-    console.log('user',user);
-    return Auth.currentAuthenticatedUser()
-  }).then((cognito_user) => {
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
-  })
-  .catch((err) => console.log(err));
-};
+  const checkAuth = async () => {
+    Auth.currentAuthenticatedUser({
+      // Optional, By default is false. 
+      // If set to true, this call will send a 
+      // request to Cognito to get the latest user data
+      bypassCache: false 
+    })
+    .then((user) => {
+      console.log('user',user);
+      return Auth.currentAuthenticatedUser()
+    }).then((cognito_user) => {
+        setUser({
+          display_name: cognito_user.attributes.name,
+          handle: cognito_user.attributes.preferred_username
+        })
+    })
+    .catch((err) => console.log(err));
+  };
+  
+  React.useEffect(()=>{
+    //prevents double call
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
 
+    loadData();
+    checkAuth();
+  }, [])
 
-
-React.useEffect(()=>{
-  //prevents double call
-  if (dataFetchedRef.current) return;
-  dataFetchedRef.current = true; 
-
-  loadData();
-  checkAuth();
-}, [])
-
-return (
-  <article>
-    <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
-    <div className='content'>
-      <ActivityForm  
-        popped={popped}
-        setPopped={setPopped} 
-        setActivities={setActivities} 
-      />
-      <ReplyForm 
-        activity={replyActivity} 
-        popped={poppedReply} 
-        setPopped={setPoppedReply} 
-        setActivities={setActivities} 
-        activities={activities} 
-      />
-      <ActivityFeed 
-        title="Home" 
-        setReplyActivity={setReplyActivity} 
-        setPopped={setPoppedReply} 
-        activities={activities} 
-      />
-    </div>
-    <DesktopSidebar user={user} />
-  </article>
-);
+  return (
+    <article>
+      <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
+      <div className='content'>
+        <ActivityForm  
+          popped={popped}
+          setPopped={setPopped} 
+          setActivities={setActivities} 
+        />
+        <ReplyForm 
+          activity={replyActivity} 
+          popped={poppedReply} 
+          setPopped={setPoppedReply} 
+          setActivities={setActivities} 
+          activities={activities} 
+        />
+        <ActivityFeed 
+          title="Home" 
+          setReplyActivity={setReplyActivity} 
+          setPopped={setPoppedReply} 
+          activities={activities} 
+        />
+      </div>
+      <DesktopSidebar user={user} />
+    </article>
+  );
 }
